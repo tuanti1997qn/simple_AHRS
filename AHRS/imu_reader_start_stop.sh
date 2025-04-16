c5fcac42559c4de139902585a73227eb6397b355
Tuan Pham <tuanti1997qn@gmail.com>
2025-04-15 19:54:11 -0700

    Add load driver

--- AHRS/imu_reader_start_stop.sh
+++ AHRS/imu_reader_start_stop.sh
@@ -4,6 +4,7 @@ case "$1" in
         echo "Usage: imu_reader_start_stop.sh [start|stop]"
         ;;
     start)
+        /lib/modules/6.1.46/extra/imu_reader_load
         /usr/bin/AHRS -d
         ;;
     stop)
